2f87460e96b8e67b51b989142e07eab67c46fefc
ar00gula <arianna.mayer@gmail.com>
2021-03-17 16:42:53 -0400

    sort function working in console

--- src/components/Books/BookListContainer.js
+++ src/components/Books/BookListContainer.js
@@ -10,13 +10,20 @@ function BookListContainer() {
     const allBooks = useSelector(selectAllBooks);
     // const filteredBooks = useSelector(selectFilteredBooks)
 
-const sortBooks = () => {
+const sortBooks = (attribute) => {
+    const splitAttribute = attribute.split(" ")
     let toSort = allBooks.books.map(book => book)
     let sortedBooks = toSort.sort(function(a, b) {
-        let sortA = a["title"].toUpperCase();
-        let sortB = b["title"].toUpperCase();
+    if (splitAttribute.length === 1) {
+        let sortA = a[attribute].toUpperCase();
+        let sortB = b[attribute].toUpperCase();
         return (sortA < sortB) ? -1 : (sortA > sortB) ? 1 : 0;
-        })
+    } else {
+        let sortA = a[splitAttribute[0]][splitAttribute[1]].toUpperCase();
+        let sortB = b[splitAttribute[0]][splitAttribute[1]].toUpperCase();
+        return (sortA < sortB) ? -1 : (sortA > sortB) ? 1 : 0;
+        }
+    })
     return sortedBooks
 }
 
@@ -28,9 +35,7 @@ const sortBooks = () => {
     
     return (
         <div>
-            {console.log(sortBooks())}
             {/* {consoleLogTHIS()}
-            {console.log(sortBooks("author"))} */}
             {/* <Filter selectFilter={handleFilterSelection}/> */}
             <BookList filter={allBooks}/>
         </div>
